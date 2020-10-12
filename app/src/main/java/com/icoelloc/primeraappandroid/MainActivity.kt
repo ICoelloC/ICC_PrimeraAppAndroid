3abc25b8e814d3db3339ef57fd1120b51c6f8685
Ivan <ivancoellocortes@gmail.com>
2020-10-12 20:33:07 +0200

    Commit on feature's branch, modified README.md, and a few things on MainActivity.kt, comments have been added with links to the Android documentation, where you can find the functions that have been implemented in the project.

--- app/src/main/java/com/icoelloc/primeraappandroid/MainActivity.kt
+++ app/src/main/java/com/icoelloc/primeraappandroid/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.CalendarContract
 import android.provider.ContactsContract
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -46,6 +45,10 @@ class MainActivity : AppCompatActivity() {
                 menuCrearEvento()
                 true
             }
+            R.id.menu_notas -> {
+                menuCrearNota()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -55,20 +58,28 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, AcercaDeActivity::class.java)
         startActivity(intent)
     }
-
-    //Accedemos a la Activity de Contactos
+    //Creamos un contacto.
     private fun menuContactos(){
         crearContacto()
     }
-    //Enviamos un correo
+    //Enviamos un correo.
     private fun menuEnviarCorreo() {
         mandarCorreo("alumnoIvanCoello@alumno.com", "Práctica", "Espero aprobar esta práctica")
     }
-
+    //Creamos un evento
     private fun menuCrearEvento(){
         crearEvento()
     }
+    //Creamos una nota
+    private fun menuCrearNota(){
+        crearNota()
+    }
 
+    /**
+     * Método que inicia la aplicación de Contactos para crear directamente un contacto.
+     *
+     * Link al api de Android--> https://developer.android.com/guide/components/intents-common?hl=es#InsertContact
+     */
     fun crearContacto() {
         val intent = Intent(Intent.ACTION_INSERT).apply {
             type = ContactsContract.Contacts.CONTENT_TYPE
@@ -78,11 +89,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Método para enviar un correo, en este caso
+     * @param receptor --> este sería el correo de la persona a la que le queremos envía un correo.
+     * @param asunto --> este es el asunto del correo.
+     * @param mensaje --> este sería el mensaje que querríamos envíar al receptor.
+     *
+     * Link al api de Android--> https://developer.android.com/guide/components/intents-common?hl=es#Email
+     */
     private fun mandarCorreo(receptor: String, asunto: String, mensaje: String) {
         val intent = Intent(Intent.ACTION_SEND)
         intent.data = Uri.parse("mailto:")
         intent.type = "text/plain"
-
         intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(receptor))
         intent.putExtra(Intent.EXTRA_SUBJECT, asunto)
         intent.putExtra(Intent.EXTRA_TEXT, mensaje)
@@ -91,9 +109,13 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
         }
-
     }
 
+    /**
+     * Método que inicia la aplicación de Google Calendar para crear directamente un evento
+     *
+     * Link al api de Android--> https://developer.android.com/guide/components/intents-common?hl=es#AddEvent
+     */
     fun crearEvento() {
         val intent = Intent(Intent.ACTION_INSERT).apply {
             data = CalendarContract.Events.CONTENT_URI
@@ -101,6 +123,10 @@ class MainActivity : AppCompatActivity() {
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
         }
+    }
+
+    fun crearNota() {
+
     }
 
 }
